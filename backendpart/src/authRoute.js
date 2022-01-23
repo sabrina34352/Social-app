f313467da64f837d3c361d0772093a5bbb238c13
sabrina34352 <s.babakulova@student.inha.uz>
2022-01-23 13:08:22 +0500

    errors

---
+++ backendpart/src/authRoute.js
@@ -0,0 +1,44 @@
+import {users} from '../../public/data.json';
+import express from 'express'
+const router = new express.Router();
+
+//for users to log in
+router.post('/login', (req, res) => {
+  try {
+    var hasMatch = false;
+    function newUserAddition() {
+      users.push({ tagname, username, email });
+      res.status(200).json(users);
+    }
+    const { tagname, username, email } = req.body;
+    for (var index = 0; index < users.length; ++index) {
+      var oldUser = users[index];
+
+      if (oldUser.tagname === tagname) {
+        hasMatch = true;
+        break;
+      }
+    }
+    hasMatch
+      ? res
+          .status(422)
+          .send('sorry! this tag is already being used! try another tagname')
+      : newUserAddition();
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+router.get('/login', (req, res) => {
+  try {
+    var tagnames = [];
+    users.forEach(user => {
+      tagnames.push(user.tagname);
+    });
+    res.status(200).json(tagnames);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+module.exports = router;
--- backendpart/src/server.js
+++ backendpart/src/server.js
@@ -1,22 +1,25 @@
 import express from 'express';
-import session from 'express-session';
+// import session from 'express-session';
 import bodyParser from 'body-parser';
 import { comments } from '../../public/data.json';
-import { users } from '../../public/data.json';
+import {authRoute} from './authRoute';
 const app = express();
-
+app.use(express.json());
 app.use(bodyParser.json());
 
 
 
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  session({
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    secret: 'shhhh, very secret',
-  })
-);
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use(
+//   session({
+//     resave: false, // don't save session if unmodified
+//     saveUninitialized: false, // don't create session until something stored
+//     secret: 'shhhh, very secret',
+//   })
+//   );
+  
+app.use("/auth", authRoute);
 
 app.post('/api/posting/add-comment', (req, res) => {
   const { username, tagname, heading, text } = req.body;
@@ -31,39 +34,5 @@ app.get('/api/posting/add-comment', (req, res) => {
 });
 
 
-//for users to log in
-app.post('/api/home/login', (req, res) => {
-
-  const { tagname, username, email } = req.body;
-  var hasMatch = false;
-
-  for (var index = 0; index < users.length; ++index) {
-
-    var oldUser = users[index];
-
-    if (oldUser.tagname === tagname) {
-      hasMatch = true;
-      break;
-    }
-  }
-
-  function newUserAddition() {
-    users.push({ tagname, username, email });
-    res.status(200).send(users);
-  }
-
-  hasMatch
-    ? res.status(422).send('sorry! this tag is already being used! try another tagname')
-    : newUserAddition();
-
-});
-
-app.get('/api/home/login', (req, res) => {
-  var tagnames = [];
-  users.forEach(user => {
-    tagnames.push(user.tagname)
-  });
-  res.status(200).send(tagnames);
-});
 
 app.listen(8000, () => console.log('listening on port 8000'));
