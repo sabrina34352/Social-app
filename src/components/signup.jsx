3abf4933c20b8fd426148e423dad506e050597c2
sabrina34352 <s.babakulova@student.inha.uz>
2022-02-17 15:33:05 +0500

    fixed sign up

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from 'react';
-import { Box, Text, Input, Button, Center } from '@chakra-ui/react';
+import {
+  Box,
+  Text,
+  Input,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+} from '@chakra-ui/react';
 
 function Signup() {
-  const [errexist, setErrexists] = useState(false);
   const [error, setError] = useState({});
   const [tagname, setTagname] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(`Email: ${email} & Password: ${password}`);
+  };
 
   async function addUsers() {
     await fetch('http://localhost:8000/auth/registration', {
@@ -26,14 +37,12 @@ function Signup() {
             .json()
             .then(data => {
               setError(data);
-              setErrexists(true);
             })
             .catch(err => console.log(err));
         } else {
           res.json();
           setError({});
         }
-
       })
       .catch(err => console.log('something went wrong', err));
   }
@@ -54,63 +63,56 @@ function Signup() {
         <Text textAlign="center" fontSize="30px">
           Sign Up
         </Text>
-        <Box m="0 2em">
-          <Text m={2}>UserName</Text>
-          <Input
-            // isInvalid={Object.keys(error).length !== 0 ? true : false}
-            isInvalid={errexist}
-            placeholder="Enter your name"
-            value={username}
-            onChange={event => {setUsername(event.target.value); setErrexists(false)}}
-          />
-          <Text m={2}>TagName</Text>
-          <Input
-          isInvalid={errexist}
-            placeholder="Enter the tagname"
-            value={tagname}
-            onChange={event =>{
-              setTagname(event.target.value)
-              setErrexists(false); 
-            }} 
-          />
-          <Text m={2}>Email address</Text>
-          <Input
-          isInvalid={errexist}
-            placeholder="Enter your email address"
-            value={email}
-            onChange={event =>{
-              setEmail(event.target.value);
-              setErrexists(false);
-            } }
-          />
-          <Text m={2}>Password</Text>
-          <Input
-          isInvalid={errexist}
-            placeholder="Enter your password"
-            value={password}
-            onChange={event => {setPassword(event.target.value); setErrexists(false)}}
-          />
-          {errors()}
-          <Center mt={7}>
-            <Button
-              variant="solid"
-              p="20px 30px"
-              onClick={() => {
-                addUsers();
-                if (Object.keys(error).length !== 0 || errexist) {
-                  return false;
-                } else {
-                  setEmail('');
-                  setPassword('');
-                  setUsername('');
-                  setTagname('');
-                }
+        <form onSubmit={handleSubmit}>
+          <FormControl isRequired>
+            <FormLabel htmlFor="user-name">User name</FormLabel>
+            <Input
+              placeholder="Enter your name"
+              value={username}
+              onChange={event => {
+                setUsername(event.target.value);
               }}
-            >
-              Sign up
-            </Button>
-          </Center>
-        </Box>
+            />
+            <FormLabel htmlFor="tag-name">Tag name</FormLabel>
+            <Input
+              placeholder="Enter the tagname"
+              value={tagname}
+              onChange={event => {
+                setTagname(event.target.value);
+              }}
+            />
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              placeholder="Enter your email address"
+              value={email}
+              onChange={event => {
+                setEmail(event.target.value);
+              }}
+            />
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              placeholder="Enter your password"
+              value={password}
+              onChange={event => {
+                setPassword(event.target.value);
+              }}
+            />
+
+            {errors()}
+            <Center mt={7}>
+              <Button
+                type="submit"
+                variant="solid"
+                p="20px 30px"
+                onClick={() => {
+                  addUsers();
+                }}
+              >
+                Sign up
+              </Button>
+            </Center>
+          </FormControl>
+        </form>
       </Box>
     </>
   );
